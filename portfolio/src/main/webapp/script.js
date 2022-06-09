92f3aedb2a2b9b56d43503274a59a2e7bd3495c3
“Jessie <jluo@sps-program.com>
2022-06-09 17:44:49 +0000

    add date fetch and doGet

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -30,3 +30,13 @@ function addRandomGreeting() {
 	const greetingContainer = document.getElementById('greeting-container');
 	greetingContainer.innerText = greeting;
 }
+
+/** Fetches the current date from the server and adds it to the page. */
+async function showServerTime() {
+    const responseFromServer = await fetch('/date');
+    const textFromResponse = await responseFromServer.text();
+  
+    const dateContainer = document.getElementById('date-container');
+    dateContainer.innerText = textFromResponse;
+}
+  
